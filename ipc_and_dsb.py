7cc508b9af4c264ff25ea3161a1eb08bad5d5993
Litrin J <litrin@gmail.com>
2020-03-09 19:55:33 +0800

    Combine IPC and DSB monitor

--- ipc_and_dsb.py
+++ ipc_and_dsb.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
-from qwer.perf import MetricCalculator, PerfEventMonitor
-from qwer.perf import BasePerfMetric
 import logging
+
+from qwer.perf import BasePerfMetric
+from qwer.perf import MetricCalculator, PerfEventMonitor
 
 logging.basicConfig(level=logging.DEBUG)
 logging.debug("Enable debugging mode")
@@ -16,11 +17,19 @@ class IPC(BasePerfMetric):
         return self["instructions"] / self["cycles"]
 
 
+class DSB_Ratio(BasePerfMetric):
+    events = ("cpu/event=0x0e,umask=0x01,name='UOPS_ISSUED.ANY'/",
+              "cpu/event=0x79,umask=0x08,name='IDQ.DSB_UOPS'/")
+
+    def calculate(self):
+        return 100 * self["IDQ.DSB_UOPS"] / self["UOPS_ISSUED.ANY"]
+
+
 if __name__ == "__main__":
     monitor = PerfEventMonitor()
 
     monitor.set_interval(time_interval=1)  # refresh every 1 second.
-    monitor.set_event_list(perf_metrics=IPC)
+    monitor.set_event_list(perf_metrics=[IPC, DSB_Ratio])
 
     process = MetricCalculator()
     monitor.set_processor(process)
